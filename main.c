6e2a3f95730cc5195a2582fc1981ac66960170d8
aursunAjay <ajay15014@gmail.com>
2023-08-06 18:47:52 -0400

    make file created

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
-
-_Alignas(4) const __UINT8_TYPE__ data  = 0;
-_Alignas(4) const __UINT8_TYPE__ data1 = 0;
-_Alignas(4) const __UINT8_TYPE__ data2 = 0;
-_Alignas(4) int data_a [10] __attribute((section(".data")));
  
 int main(void)
-{   
-   auto     int auto_local;    
-   register int register_;    
+{    
+    S 
    return 0;
 }
