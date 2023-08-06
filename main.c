bd18d544dda21e677573caa267640abe06bd4517
aursunAjay <ajay15014@gmail.com>
2023-08-06 18:13:49 -0400

    updated

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-int      init_  = 10; 
-int      global;  
-const    int const_;
-extern   int extern_; 
-static   int static_; 
-volatile int volatile_;
 
+_Alignas(4) const __UINT8_TYPE__ data  = 0;
+_Alignas(4) const __UINT8_TYPE__ data1 = 0;
+_Alignas(4) const __UINT8_TYPE__ data2 = 0;
+_Alignas(4) int data_a [10] __attribute((section(".data")));
+ 
 int main(void)
 {   
    auto     int auto_local;    
