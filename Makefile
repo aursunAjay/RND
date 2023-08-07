85fd8841c812a461281c2768107db400ce7daf14
aursunAjay <ajay15014@gmail.com>
2023-08-06 20:09:19 -0400

    Updated : problem was main.bin was not created now working due to change in make file for generate bin file

--- Makefile
+++ Makefile
@@ -1,19 +1,25 @@
-convert:
-	arm-none-eabi-objcopy -O binary main.elf main.bin
 
-clean:
-	 
-	rm -f main.o startup.o main.elf syscall.o main.map main_log main.bin
+CC=arm-none-eabi-gcc
+MACH=cortex-m4
+LDFLAGS		= -mcpu=$(MACH) -mthumb -mfloat-abi=soft --specs=nano.specs -T linker.ld -Wl,-Map=main.map
+LDFLAGS_SH	= -mcpu=$(MACH) -mthumb -mfloat-abi=soft --specs=rdimon.specs -T linker.ld -Wl,-Map=main.map
+
+ 
+clear: 
+	rm -rf *.o *.elf *.map *.bin
 
 compile:
-	arm-none-eabi-gcc  -c -mcpu=cortex-m4  -mthumb -std=gnu11 -O0   *.c
-	arm-none-eabi-gcc -nostdlib -T linker.ld  -Wl,-Map=main.map -o main.elf  *.o
+	arm-none-eabi-gcc  -c -mcpu=cortex-m4 -mthumb-interwork -mthumb -std=gnu11 -O0  *.c
+	echo "Compiled\n"
+	$(CC) $(LDFLAGS) -o main.elf  *.o
+	echo "Linked\n"
 	arm-none-eabi-objdump -h main.elf > main_log 
+	arm-none-eabi-objcopy -O binary main.elf main.bin 	
 	arm-none-eabi-size -tA main.elf
 
-
 compile1:
-	arm-none-eabi-gcc  -o main.elf *.c    --specs=nosys.specs -std=gnu11 -g -O0   -mlittle-endian -mthumb -mthumb-interwork -mcpu=cortex-m4 -fsingle-precision-constant -Wdouble-promotion 
+	
+	#arm-none-eabi-gcc  -o main.elf *.c --specs=nano.specs -std=gnu11   -O0   -mlittle-endian -mthumb -mthumb-interwork -mcpu=cortex-m4 -fsingle-precision-constant -Wdouble-promotion 
  
 upload:
 	st-flash write main.bin 0x8000000
--- main.c
+++ main.c
@@ -1,7 +1,91 @@
 #include <stdio.h>
- 
+#include <stdint.h>
+#define LED_GREEN  12
+#define LED_ORANGE 13
+#define LED_RED    14
+#define LED_BLUE   15
+
+#define DELAY_COUNT_1MS 		 1250U
+#define DELAY_COUNT_1S  		(1000U * DELAY_COUNT_1MS)
+#define DELAY_COUNT_500MS  		(500U  * DELAY_COUNT_1MS)
+#define DELAY_COUNT_250MS 		(250U  * DELAY_COUNT_1MS)
+#define DELAY_COUNT_125MS 		(125U  * DELAY_COUNT_1MS)
+
+
+void led_on(uint8_t led_no)
+{
+  uint32_t *pGpiodDataReg = (uint32_t*)0x40020C14;
+  *pGpiodDataReg |= ( 1 << led_no);
+
+}
+
+void led_off(uint8_t led_no)
+{
+	  uint32_t *pGpiodDataReg = (uint32_t*)0x40020C14;
+	  *pGpiodDataReg &= ~( 1 << led_no);
+
+}
+
+ void delay(uint32_t count)
+{
+  for(uint32_t i = 0 ; i < count ; i++)
+  {
+    for(int a=0;a<100;a++);
+  }
+}
+
+void led_init_all(void)
+{
+
+	uint32_t *pRccAhb1enr = (uint32_t*)0x40023830;
+	uint32_t *pGpiodModeReg = (uint32_t*)0x40020C00;
+
+
+	*pRccAhb1enr |= ( 1 << 3);
+	//configure LED_GREEN
+	*pGpiodModeReg |= ( 1 << (2 * LED_GREEN));
+	*pGpiodModeReg |= ( 1 << (2 * LED_ORANGE));
+	*pGpiodModeReg |= ( 1 << (2 * LED_RED));
+	*pGpiodModeReg |= ( 1 << (2 * LED_BLUE));
+
+#if 0
+	//configure the outputtype
+	*pGpioOpTypeReg |= ( 1 << (2 * LED_GREEN));
+	*pGpioOpTypeReg |= ( 1 << (2 * LED_ORANGE));
+	*pGpioOpTypeReg |= ( 1 << (2 * LED_RED));
+	*pGpioOpTypeReg |= ( 1 << (2 * LED_BLUE));
+#endif
+
+    led_off(LED_GREEN);
+    led_off(LED_ORANGE);
+    led_off(LED_RED);
+    led_off(LED_BLUE);
+
+
+
+}
+
+
 int main(void)
 {    
- 
-   return 0;
+    led_init_all();
+
+ while(1)
+  {
+
+     led_on(LED_RED);
+    led_on(LED_GREEN);
+    led_on(LED_ORANGE);
+    led_on(LED_RED);
+    led_on(LED_BLUE);
+    delay(500);
+   
+    led_off(LED_GREEN);
+    led_off(LED_ORANGE);
+    led_off(LED_RED);
+    led_off(LED_BLUE);
+     delay(500);
+   
+ }
+      return 0;
 }
